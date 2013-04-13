95f6961c2d1e06a11bd2328b6166c3d1261bfea9
Jonathan Lutz <jllu223@g.uky.edu>
2013-04-13 19:25:53 -0400

    Messing up the cache

--- ProcessorComponents/DMCache/DMCache.v
+++ ProcessorComponents/DMCache/DMCache.v
@@ -18,18 +18,25 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module DMCache(readWrite, readAddr, writeAddr, dataIn, clk, clr, dataOut);
+module DMCache(readWrite, addr, dataIn, clk, clr, dataOut, ackRAM, dataInRAM, dataOutRAM, addrOutRAM, readWriteRAM, synRAM);
 	parameter blocksize = 1;
 	parameter ramWidth = 8; //These need to be used to set up reg array
+	parameter addrWidth = 8;
 	parameter lineSize = 13; //These need to be used to set up reg array
 	parameter blockAddrBits = 4;
-	input readWrite, clk, clr; // read if 0 write if 1
-	input [ramWidth-1:0] readAddr, writeAddr, dataIn; //Should maybe simplify to only be one thing for address instead of two kinds of addresses???
 	
-	output [ramWidth-1:0] dataOut;
+	input readWrite, clk, clr; // read if 0 write if 1
+	input [ramWidth-1:0] addr, dataIn; //Should maybe simplify to only be one thing for address instead of two kinds of addresses???
+	output reg[ramWidth-1:0] dataOut;
+	
+	//Inputs/Outputs to and from RAM
+	input ackRAM;
+	input [ramWidth-1:0] dataInRAM;
+	output reg [ramWidth-1:0] dataOutRAM;
+	output reg [addrWidth-1:0] addrOutRAM;
+	output reg synRAM;
 	
 	integer i; // Used for clearing cache
-
 	reg [lineSize-1:0] regArray [(2**blockAddrBits)-1:0];
 	
 
@@ -39,41 +46,52 @@ module DMCache(readWrite, readAddr, writeAddr, dataIn, clk, clr, dataOut);
 		begin
 			for(i=0; i<2**blockAddrBits; i=i+1)
 			begin
-				regArray[i] = 0;
+				regArray[i] <= 0;
 			end
 		end				
 		else if(readWrite==1'b1)
 		begin
-			if(regArray[writeAddr[ramWidth-1:blockAddrBits]][lineSize-2:ramWidth] == writeAddr[ramWidth-1:blockAddrBits]) begin //HIT!!!
-				regArray[writeAddr[ramWidth-1:blockAddrBits]] = dataIn;
+			if(regArray[addr[ramWidth-1:blockAddrBits]][lineSize-2:ramWidth] == addr[ramWidth-1:blockAddrBits]) begin //HIT!!!
+				regArray[addr[ramWidth-1:blockAddrBits]] <= {1'b1, addr[addrWidth-1:blockAddrBits], dataIn}; //Address should be the same, but why not assign again to make 1 clean line
+//This sets this line of the cache as dirty
 			end
 			else begin // MISS!!!
-				if(regArray[writeAddr[ramWidth-1:blockAddrBits]][lineSize-1] == 1'b0) begin //this means it is clean (not dirty)
-					regArray[writeAddr[ramWidth-1:blockAddrBits]] = dataIn;
+				if(regArray[addr[ramWidth-1:blockAddrBits]][lineSize-1] == 1'b0) begin //this means it is clean (not dirty)
+					regArray[addr[ramWidth-1:blockAddrBits]] <= {1'b1, addr[addrWidth-1:blockAddrBits], dataIn}; //Makes dirty, addrCheck, and then writes the data
 				end
 				else begin // This is dirty
 					//Implement some logic to write data out to RAM
-					regArray[writeAddr[ramWidth-1:blockAddrBits]] = dataIn;
+					addrOutRAM <= {regArray[addr[addrWidth-1:blockAddrBits]][lineSize-2:ramWidth], addr[blockAddrBits-1:0]};
+					dataOutRAM <= regArray[addr[addrWidth-1:blockAddrBits]][ramWidth-1:0];
+					sendRAM <= 1'b1;					
+					//End logic of writing data to RAM
+					//FIND WAY TO LOOP UNTIL RAM SENDS ACK
+					//regArray[addr[ramWidth-1:blockAddrBits]] <= {1'b1, addr[addrWidth-1:blockAddrBits], dataIn};
 				end
 			end
 		end
 		else if(readWrite==1'b0) begin
-			if(regArray[writeAddr[ramWidth-1:blockAddrBits]][lineSize-2:ramWidth] == writeAddr[ramWidth-1:blockAddrBits]) begin //HIT!!!
-				dataOut = regArray[writeAddr[ramWidth-1:blockAddrBits]];
+			if(regArray[addr[ramWidth-1:blockAddrBits]][lineSize-2:ramWidth] == addr[addrWidth-1:blockAddrBits]) begin //HIT!!!
+				dataOut <= regArray[addr[addrWidth-1:blockAddrBits]][ramWidth-1:0];
 			end
 			else begin // MISS!!!
-				if(regArray[writeAddr[ramWidth-1:blockAddrBits]][lineSize-1] == 1'b0) begin //this means it is clean (not dirty)
+				if(regArray[addr[addrWidth-1:blockAddrBits]][lineSize-1] == 1'b0) begin //this means it is clean (not dirty)
 					//implement logic to retrieve data from RAM to this line of the cache
-					dataOut = regArray[writeAddr[ramWidth-1:blockAddrBits]];
+					dataOut <= regArray[addr[addrWidth-1:blockAddrBits]][ramWidth-1:0];
 				end
 				else begin // This is dirty
+					//Implement some logic to write data out to RAM
+					readWriteRAM <= 1'b1;
+					addrOutRAM <= addr;
+					dataOutRAM <= regArray[addr[addrWidth-1:blockAddrBits]][ramWidth-1:0];
+					sendRAM <= 1'b1;					
+					//End logic of writing data to RAM
 					//Implement some logic to write data out to RAM and then load data from RAM to this place of cache
-					dataOut = regArray[writeAddr[ramWidth-1:blockAddrBits]];
+					dataOut <= regArray[addr[addrWidth-1:blockAddrBits]][ramWidth-1:0];
 				end
 			end			
 		end
 	end
-	assign dataOut = regArray[readAddr];
 endmodule
 
 
